12e34d7043849c46b6aca6e241d7905ee243368b
Maria Eduarda Vieira da Costa <116650361+vmC0sta@users.noreply.github.com>
2023-10-20 14:35:23 -0300

    feat,refactor a e docs

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,36 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const btnGerarQRCODE = document.getElementById('btn_gerarQRCODE');
-    const urlInput = document.getElementById('url');
-    const descricaoURL = document.querySelector('.descricao__card')
-    const card = document.querySelector('.card');
-    const content = document.querySelector('.content');
-    
+const btnVoltar = document.getElementById('btn-voltar')
+const btnGerarQRCODE = document.getElementById('btn_gerarQRCODE');
+const urlInput = document.getElementById('url')
+const card = document.querySelector('.card');
+const content = document.querySelector('.content');
+const elemento_qrcode = document.getElementById('qrcode')
+
+document.addEventListener('DOMContentLoaded', () => {    
 
     btnGerarQRCODE.addEventListener('click', () => {
 
-        const url = urlInput.value;
+        const url = urlInput.value
 
         if (url) {
-            card.style.display = 'block'; 
-            content.style.display = 'none'
 
+            card.style.display = 'block'
+            content.style.display = 'none'
             var qrcode = new QRCode("qrcode", {
                 text: url,
-                width: 220,
-                height: 220
-            });
+                width: 310,
+                height: 230
+            })
         } else {
             alert("Por favor, insira uma URL antes de gerar o QRCode.");
         } 
     });
+
+    btnVoltar.addEventListener('click',()=>{
+        card.style.display = 'none'
+        content.style.display = 'block'
+        urlInput.value = ''
+        elemento_qrcode.innerHTML = ''
+
+    })
+
 });
